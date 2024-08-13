ea6b690d986289ba1acca837d0069dd7877ebc1c
Gavin Xue <gavinxue050705@gmail.com>
2024-08-13 14:38:45 -0400

    Changed port

--- server/app.js
+++ server/app.js
@@ -24,6 +24,7 @@ app.use((err, req, res, next) => {
     res.status(500).send(err)
 })
 
-app.listen('3000', () => {
-    console.log('Listening on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
